397c99b219627d7793ba5ce60286e0a76e067d9c
Hastyn <81477318+Hastyn@users.noreply.github.com>
2023-03-22 19:11:49 +0530

    Add files via upload

--- nuke/visualize.py
+++ nuke/visualize.py
@@ -1,14 +1,10 @@
-from treelib import Tree
-import pydot
-import graphviz
-
 from treelib import Tree, Node
 import sys
 import subprocess
 import pydot
 
 
-def show1(file_name):
+def show(file_name):
     
     f = open(file_name,'r')
     
@@ -37,43 +33,8 @@ def show1(file_name):
     # f.close()
 
     (graph,) = pydot.graph_from_dot_file(file_name[:-3]+"dot")
-    graph.write_png(file_name[:-3]+'png')
+    graph.write_png(file_name[:-3]+'.png')
 
     # subprocess.check_call(['rm','temp.dot'])
     # subprocess.check_call(['rm',file_name])
-
-def show(file_name):
-    f = open(file_name, 'r')
-    lines = f.readlines()
-    f.close()
-    tree = Tree()
-
-    for line in lines:
-        line = line.strip().split()
-        if line[1] == '-1':
-            if line[3] == '0':
-                tree.create_node(line[2], line[0], data={'color': 'red'})
-            else:
-                tree.create_node(line[2], line[0], data={'color': 'blue'})
-        else:
-            if line[3] == '0':
-                tree.create_node(f"{line[2]}\n{line[0]}\n{line[3]}", line[0], line[1], data={'color': 'red'})
-            else:
-                tree.create_node(f"{line[2]}\n{line[0]}\n{line[3]}", line[0], line[1], data={'color': 'blue'})
-
-    dot = graphviz.Digraph(comment='Tree')
-    for node in tree.all_nodes():
-        color = node.data.get('color')
-        if color:
-            dot.node(node.identifier, label=node.tag, color=color, style='filled')
-        else:
-            dot.node(node.identifier, label=node.tag)
-
-    for node in tree.all_nodes():
-        for child_id in tree.children(node.identifier):
-            # print(node.identifier,child_id)
-            dot.edge(node.identifier, child_id.identifier)
-
-    dot.render(file_name[:-3] + 'dot', view=False)
-    (graph,) = pydot.graph_from_dot_file(file_name[:-3] + 'dot')
-    graph.write_png(file_name[:-3] + 'png')
+    
